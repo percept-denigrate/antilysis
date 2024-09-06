3cc8ff995054f7ba7c6a1c9199c746a75577e31d
othalan <othalan>
2024-09-06 10:29:48 +0200

    wait for left click

--- examples/test.rs
+++ examples/test.rs
@@ -4,4 +4,6 @@ fn main() {
     println!("Processes: {}", antilysis::processes());
     println!("Sandbox: {}", antilysis::sandbox());
     println!("Detected: {}", antilysis::detected());
+    antilysis::wait_for_left_click();
+    println!("Left click");
 }
