231a3253b95ce0748f2fbd03e872f1e0cd3b87d7
othalan <othalan>
2024-10-21 14:59:20 +0200

    solved conflict

--- examples/test.rs
+++ examples/test.rs
@@ -7,14 +7,7 @@ fn main() {
     println!("Processes: {}", antilysis::processes());
     println!("Sandbox: {}", antilysis::sandbox());
     println!("Detected: {}", antilysis::detected());
-<<<<<<< HEAD
-<<<<<<< HEAD
-}
-=======
-    antilysis::wait_for_left_click();
-=======
+    
     antilysis::wait_for_left_click(2);
->>>>>>> af224ff (multiple clicks)
     println!("Left click");
 }
->>>>>>> 3cc8ff9 (wait for left click)
